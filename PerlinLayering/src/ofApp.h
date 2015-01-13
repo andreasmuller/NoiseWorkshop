04c92a918303b3f8bc10485234ea79d1c44548be
Andreas Müller <andreas@nanikawa.com>
2015-01-13 19:54:50 +0000

    no message

--- PerlinLayering/src/ofApp.h
+++ PerlinLayering/src/ofApp.h
@@ -7,11 +7,13 @@ class ofApp : public ofBaseApp
 {
 	public:
 	
+	
+	// Fractal brownian motion
+	
 		// --------------------------------
 		void setup()
 		{
 			fontSmall.loadFont("Fonts/DIN.otf", 8 );
-			
 			
 			ofxGuiSetFont( "Fonts/DIN.otf", 8 );
 			ofxGuiSetDefaultWidth( 260 );
@@ -19,9 +21,9 @@ class ofApp : public ofBaseApp
 			string xmlSettingsPath = "Settings/PerlinLayering.xml";
 			gui.setup( "Perlin Layers", xmlSettingsPath );
 			
-			gui.add( numOctaves.set("Num Octaves",					1,  1,  10) );
-			gui.add( baseFrequency.set("Base Frequency",		    0.01f,  0.001f,  0.2f) );
-			gui.add( persistence.set("Persistence",					2.0f,  0.001f,  0.2f) );
+			gui.add( numOctaves.set("Num Octaves",			  1,  1,		10) );
+			gui.add( baseFrequency.set("Base Frequency",  0.01f,  0.0001f,  0.05f) ); // As we are feeding values measured in pixels, the frequency value will be pretty small
+			gui.add( persistence.set("Persistence",		   0.1f,  0.001f,   1.0f) );
 			
 			gui.loadFromFile( xmlSettingsPath );
 			
@@ -31,9 +33,6 @@ class ofApp : public ofBaseApp
 		// --------------------------------
 		void update()
 		{
-			// As we are feeding values measured in pixels, the frequency value will be pretty small
-			//float frequency = ofMap( ofGetMouseX(),  0, ofGetWidth(),  0.0001, 0.05 );
-			
 			int res = 256;
 
 			// clear and allocate images if needed
@@ -47,52 +46,40 @@ class ofApp : public ofBaseApp
 			
 			noiseResult.allocate( res, res, OF_IMAGE_GRAYSCALE );
 
-			ofFloatPixelsRef pix = noiseResult.getPixelsRef();
+
 
 			// Compute noise
+			
+			ofFloatPixelsRef pix = noiseResult.getPixelsRef();
+			
 			int tmpIndex = 0;
 			for( int y = 0; y < res; y++ )
 			{
 				for( int x = 0; x < res; x++ )
 				{
-					float total = 0.0f;
+					float totalAmplitude = 0.0f;
+					float finalValue = 0.0f;
 					for( int i = 0; i < numOctaves; i++ )
 					{
-						//float frequency = (i * 2) * baseFrequency;
 						float frequency = powf(2.0, i) * baseFrequency;
-						float amplitude = powf(persistence, i); //p * i;
+						float amplitude = powf(persistence, i);
 						
-						float layerVal = ofNoise( x * frequency, y * frequency );
-						total += layerVal * amplitude;
+						totalAmplitude += amplitude; // let's store the total amplitude alue so we can normalize later
 						
-						layers.at(i).getPixelsRef()[tmpIndex] = layerVal;
+						float layerValue = ofNoise( x * frequency, y * frequency );
+						finalValue += layerValue * amplitude;
+						
+						layers.at(i).getPixelsRef()[tmpIndex] = layerValue; // store the value in a separate texture so we can have a look at it
 					}
 					
-					pix[tmpIndex] = total;
-					
-					/*
-					  total = 0
-					  p = persistence
-					  n = Number_Of_Octaves - 1
-
-					  loop i from 0 to n
-
-						  frequency = 2i
-						  amplitude = pi
-
-						  total = total + InterpolatedNoisei(x * frequency, y * frequency) * amplitude
-
-					  end of i loop
-
-					  return total
-					 */
+					pix[tmpIndex] = finalValue / totalAmplitude;
 					
 					tmpIndex++;
 				}
 			}
 			
 			// We've manually changed the pixels, so make sure we uodate
-			//for( int i = 0; i < numOctaves; i++ ) { layers.at(i).update(); }
+			for( int i = 0; i < numOctaves; i++ ) { layers.at(i).update(); }
 			noiseResult.update();
 			
 		}
@@ -100,13 +87,29 @@ class ofApp : public ofBaseApp
 		// --------------------------------
 		void draw()
 		{
+			float w = ofGetWidth();
+			float h = ofGetHeight();
+			ofRectangle screenRect(0,0,w,h);
+			
 			ofBackgroundGradient( ofColor(55), ofColor(0), OF_GRADIENT_CIRCULAR );
 			
-			ofEnableAlphaBlending();
+			ofRectangle resultDrawRect( 0, 0, noiseResult.getWidth(), noiseResult.getHeight() );
+			resultDrawRect.scaleTo( screenRect, OF_SCALEMODE_CENTER );
 			
-			noiseResult.draw(0,0);
+			noiseResult.draw( resultDrawRect );
 			
-			//cout << noiseResult.getWidth() << ", " << noiseResult.getHeight() << endl;
+			if( numOctaves > 1 )
+			{
+				float border = 3;
+				//float layerDrawSize = MIN( (h - (border * layers.size())) / layers.size(), noiseResult.getHeight() + (border*2) );
+				float layerDrawSize = (h - (border * (layers.size()+1))) / layers.size();
+				
+				for( unsigned int i = 0; i < layers.size(); i++ )
+				{
+					layers.at(i).draw( w - layerDrawSize - border, ((layerDrawSize+border) * i) + border, layerDrawSize, layerDrawSize );
+				}
+			}
+			
 			
 			if( drawGui )
 			{
