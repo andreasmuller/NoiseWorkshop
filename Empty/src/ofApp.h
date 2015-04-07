44752acd300c8306fc75934a8f764baa9aa042c1
Andreas Müller <andreas@nanikawa.com>
2015-04-07 18:57:46 +0100

    - Added an empty project to start from.

---
+++ Empty/src/ofApp.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp
+{
+	public:
+	
+		// --------------------------------
+		void setup()
+		{
+
+		}
+	
+		// --------------------------------
+		void draw()
+		{
+
+		}
+	
+		// --------------------------------
+		void keyPressed( int _key )
+		{
+
+		}
+	
+};
